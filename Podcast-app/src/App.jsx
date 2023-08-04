495f769fe643497c4c8b1d516a910ce23e83739a
refilwe204 <nqatyelwaangelin@gmail.com>
2023-08-04 16:08:11 +0200

    edits

--- Podcast-app/src/App.jsx
+++ Podcast-app/src/App.jsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 import './index.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Favorite from './components/Favorite';
 import Preview from './components/Preview';
 import History from './components/History';
+import supabase from './config/SupabaseClient';
 
 
 function App() {
@@ -20,8 +21,6 @@ function App() {
     setCurrentPage(page);
   };
 
-  const [listeningHistory, setListeningHistory] = useState([]);
-
   const handleEpisodeComplete = (episode) => {
     if (!listeningHistory.some((item) => item.id === episode.id)) {
       setListeningHistory((prevHistory) => [...prevHistory, episode]);
@@ -30,7 +29,11 @@ function App() {
 
   const handleEpisodeProgress = (episode, currentTime) => {
     if (currentTime >= episode.duration - 10) {
-      // Handle episode progress
+      setLastListened({
+        show: episode.show,
+        episode: episode.title,
+        progress: currentTime,
+      });
     }
   };
 
@@ -49,56 +52,27 @@ function App() {
     localStorage.setItem('favoriteEpisodes', JSON.stringify(favorites));
   }, [favorites]);
 
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-    console.log('Logged In');
-  };
-
-  const handleLogOut = async () => {
-    try {
-      await supabase.auth.signOut();
-      setIsLoggedIn(false);
-      console.log('Logged Out');
-    } catch (error) {
-      console.error('Error logging out: ', error.message);
-    }
-  };
-
-  useEffect(() => {
-    if (isLoggedIn) {
-      setIsLoggedIn(true);
-    } else {
-      setIsLoggedIn(false);
-    }
-  }, [isLoggedIn]);
-
   return (
-    isLoggedIn ? (
-      <>
-        <Navbar onNavigate={handleNavigation} />
-        <br />
-        <br />
-        {currentPage === 'home' && (
-          <Home onPodcastClick={setSelectedPodcast} selectedPodcast={selectedPodcast} />
-        )}
-        {currentPage === 'favorite' && (
-          <Favorite favorites={favorites} setFavorites={setFavorites} />
-        )}
-        {currentPage === 'preview' && (
-          <Preview
-            podcastId={selectedPodcast?.id}
-            onFavoriteClick={handleFavoriteClick} // Pass the handleFavoriteClick function as a prop
-            onEpisodeComplete={handleEpisodeComplete}
-            onEpisodeProgress={handleEpisodeProgress}
-          />
-        )}
-        {currentPage === 'history' && <History />}
-      </>
-    ) : (
-      <Signin onLogin={handleLogin} />
-    )
+    <>
+      <Navbar onNavigate={handleNavigation} />
+      <br />
+      <br />
+      {currentPage === 'home' && (
+        <Home onPodcastClick={setSelectedPodcast} selectedPodcast={selectedPodcast} />
+      )}
+      {currentPage === 'favorite' && (
+        <Favorite favorites={favorites} setFavorites={setFavorites} />
+      )}
+      {currentPage === 'preview' && (
+        <Preview
+          podcastId={selectedPodcast?.id}
+          onFavoriteClick={handleFavoriteClick} // Pass the handleFavoriteClick function as a prop
+          onEpisodeComplete={handleEpisodeComplete}
+          onEpisodeProgress={handleEpisodeProgress}
+        />
+      )}
+      {currentPage === 'history' && <History />}
+    </>
   );
 }
 
